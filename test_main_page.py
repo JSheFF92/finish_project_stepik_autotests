f98e948592ebf487c3d1d945d210bc1508e15499
Evgenyi <shevchuk291992@gmail.com>
2023-04-12 00:25:47 +0300

    4.9 Переходы между страницами

--- test_main_page.py
+++ test_main_page.py
@@ -8,21 +8,11 @@ def test_guest_can_go_to_login_page(browser):
 
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-def test_guest_should_be_see_form_login_registration(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-
-    page.open()  # открываем страницу
-    page.should_be_login_link()
-
-    page.go_to_login_page()
-
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
