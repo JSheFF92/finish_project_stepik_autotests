1a13dfeb931c913cdb07e7f9a24edf4d8d438c6e
Evgenyi <shevchuk291992@gmail.com>
2023-04-12 00:10:57 +0300

    4.8 Реализация LoginPage

--- conftest.py
+++ conftest.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
                      help="Choose language: '--language=es/fr or .etc")
